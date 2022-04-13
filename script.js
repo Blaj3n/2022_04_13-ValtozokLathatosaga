7637cb5a87bad756b5aa5f9a6dfdd648504e3e19
Blaj3n <ulrichbence15@freemail.hu>
2022-04-13 10:58:23 +0200

    const + konstanst tömb

--- script.js
+++ script.js
@@ -15,23 +15,43 @@ function koszon() {
 /*console.log(szoveg);*/
 koszon("Halihó");
 
-for (var index = 0; index < 5; index++) {
-    
-}
 /*console.log(index);*/
 
 /**TÖMBÖK */
 
-let tomb = [12,13,14,15];
-let a=3
+let tomb = [12, 13, 14, 15];
+let a = 3;
 
-function kiir(belsoTomb, a){
-    belsoTomb[0]=11111;
-    a=3333;
-    console.log("belső a: " + a)
-    console.log("belső tömb: " + belsoTomb)
+function kiir(belsoTomb, a) {
+  belsoTomb[0] = 11111;
+  belsoTomb = [12, 23];
+  a = 3333;
+  console.log("belső a: " + a);
+  console.log("belső tömb: " + belsoTomb);
+
+  for (var index = 0; index < 5; index++) {
+    console.log(index);
+  }
+  console.log(index);
 }
 
 kiir(tomb, a);
-console.log("külső a: " + a)
-console.log("külső tömb: " + tomb)
+console.log("külső a: " + a);
+console.log("külső tömb: " + tomb);
+
+const konstans = 2;
+const konstantTomb = [23,34,45,56];
+konstantTomb[0]=111111;
+
+function masik(belsotomb2){
+    belsotomb2[2] = 234567;
+}
+masik(konstantTomb);
+
+console.log(konstantTomb);
+
+
+const szemelyObj = {
+    nev:"Béla",
+    kor: 12
+}
